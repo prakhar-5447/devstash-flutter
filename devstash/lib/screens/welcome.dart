0c0a0321c38fdd1ae0068f22c4f96d6bca405ca1
pratham-0094 <sahupratham022003@gmail.com>
2023-06-10 22:58:14 +0530

    modify style

--- devstash/lib/screens/welcome.dart
+++ devstash/lib/screens/welcome.dart
@@ -8,31 +8,51 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 117, 140, 253),
+        backgroundColor: const Color.fromARGB(255, 117, 140, 253),
         body: Padding(
-          padding: EdgeInsets.all(25),
+          padding: const EdgeInsets.all(25),
           child: Column(
             children: [
-              const Text(
-                "Welcome to DevStash\n- Your Ultimate Developer's Companion!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Comfortaa',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 25),
+              Image.asset(
+                'assets/images.jpeg',
+                width: 600,
+                fit: BoxFit.fill,
               ),
-              const Text(
-                "Unleash the power of creativity and innovation with our cutting-edge platform designed to fuel your coding journey. Whether you're a seasoned developer or just starting out",
-                style: TextStyle(
-                    color: Color.fromARGB(255, 174, 183, 254),
-                    fontFamily: 'Comfortaa',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15),
+              const Padding(
+                padding:
+                    EdgeInsets.only(left: 0, top: 100, right: 0, bottom: 100),
+                child: Column(
+                  children: [
+                    Text(
+                      "Welcome to DevStash\n- Your Ultimate Developer's Companion!",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Comfortaa',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 25),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Unleash the power of creativity and innovation with our cutting-edge platform designed to fuel your coding journey. Whether you're a seasoned developer or just starting out",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 174, 183, 254),
+                          fontFamily: 'Comfortaa',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 15),
+                    )
+                  ],
+                ),
               ),
               ElevatedButton(
                 onPressed: () {},
-                child: Padding(
-                  padding: EdgeInsets.all(15),
+                style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Color.fromARGB(255, 39, 24, 126))),
+                child: const Padding(
+                  padding:
+                      EdgeInsets.only(left: 50, top: 15, right: 50, bottom: 15),
                   child: Row(mainAxisSize: MainAxisSize.min, children: [
                     Text(
                       "Let's Start",
@@ -43,7 +63,7 @@ class WelcomeScreen extends StatelessWidget {
                           fontSize: 18),
                     ),
                     SizedBox(
-                      width: 5,
+                      width: 10,
                     ),
                     Icon(
                       Icons.arrow_right_alt,
@@ -52,9 +72,9 @@ class WelcomeScreen extends StatelessWidget {
                     ),
                   ]),
                 ),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll<Color>(
-                        Color.fromARGB(255, 39, 24, 126))),
+              ),
+              const Spacer(
+                flex: 1,
               )
             ],
           ),
