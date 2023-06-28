27ad601f3c7f42fa7ad3ecfba457d58bb9387056
pratham-094 <sahupratham022003@gmail.com>
2023-06-28 22:35:51 +0530

    bookmark page completed

--- devstash/lib/screens/saved.dart
+++ devstash/lib/screens/saved.dart
@@ -1,3 +1,5 @@
+import 'package:devstash/models/Bookmarks.dart';
+import 'package:devstash/models/ProjectList.dart';
 import 'package:flutter/material.dart';
 
 class Saved extends StatefulWidget {
@@ -10,6 +12,14 @@ class Saved extends StatefulWidget {
 class _SavedState extends State<Saved> with SingleTickerProviderStateMixin {
   late TabController _tabController;
   final List<String> _tabs = ['Favourites', 'Bookmarks'];
+  final List<ProjectList> project = [
+    ProjectList("assets/todo.png"),
+    ProjectList("assets/firstmy.jpg")
+  ];
+  final List<Bookmarks> bookmark = [
+    Bookmarks("PRATHAM SAHU", "assets/google.png"),
+    Bookmarks("PRAKHAR SAHU", "assets/google.png"),
+  ];
 
   @override
   void initState() {
@@ -50,27 +60,147 @@ class _SavedState extends State<Saved> with SingleTickerProviderStateMixin {
               ],
             ),
           )),
-      TabBar(
-        controller: _tabController,
-        tabs: _tabs.map((String tab) => Tab(text: tab)).toList(),
+      Container(
+        decoration:
+            const BoxDecoration(color: Color.fromARGB(255, 255, 255, 255)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: TabBar(
+            controller: _tabController,
+            dividerColor: Colors.transparent,
+            tabs: _tabs
+                .map((String tab) => Tab(
+                      child: Text(tab,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 75, 73, 70),
+                              fontFamily: 'Comfortaa',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14)),
+                    ))
+                .toList(),
+            indicator: const UnderlineTabIndicator(
+              borderSide: BorderSide(
+                color: Color.fromARGB(255, 117, 140, 253),
+                width: 3,
+              ),
+              insets: EdgeInsets.symmetric(horizontal: 120),
+            ),
+          ),
+        ),
       ),
       Expanded(
-        child: TabBarView(
-          controller: _tabController,
-          children: [
-            Container(
-              color: Colors.blue,
-              child: const Center(
-                child: Text('Tab 1 Content'),
+        child: Container(
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 255, 255, 255)),
+          child: TabBarView(
+            controller: _tabController,
+            children: [
+              SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 5, left: 25, right: 25),
+                  child: GridView.builder(
+                    shrinkWrap:
+                        true, // Allow the GridView to take only the necessary height
+                    physics:
+                        const NeverScrollableScrollPhysics(), // Disable GridView's scrolling
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // Number of columns in the grid
+                      crossAxisSpacing: 25,
+                      mainAxisSpacing: 25,
+                    ),
+                    itemCount: project.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                const Color.fromARGB(255, 165, 165, 165)
+                                    .withOpacity(0.25),
+                                BlendMode.darken,
+                              ),
+                              image: AssetImage(
+                                project[index].image,
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(11))),
+                      );
+                    },
+                  ),
+                ),
               ),
-            ),
-            Container(
-              color: Colors.green,
-              child: const Center(
-                child: Text('Tab 2 Content'),
-              ),
-            ),
-          ],
+              Padding(
+                  padding: const EdgeInsets.only(top: 5, left: 25, right: 25),
+                  child: ListView.builder(
+                      itemCount: bookmark.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(
+                              left: 15, right: 15, bottom: 30),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      width: 60,
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.black),
+                                        borderRadius: BorderRadius.circular(30),
+                                        color: Colors.white,
+                                      ),
+                                      child: Image.asset(
+                                        bookmark[index].avatar,
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 15),
+                                      child: Text(bookmark[index].name,
+                                          style: const TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 75, 73, 70),
+                                              fontFamily: 'Comfortaa',
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 14)),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                  child: ElevatedButton(
+                                      style: ButtonStyle(
+                                          shape: MaterialStateProperty.all<
+                                                  RoundedRectangleBorder>(
+                                              RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(3),
+                                                  side: const BorderSide(
+                                                      color: Color.fromARGB(255,
+                                                          117, 140, 253)))),
+                                          backgroundColor:
+                                              const MaterialStatePropertyAll<Color>(
+                                                  Color.fromARGB(
+                                                      255, 117, 140, 253))),
+                                      onPressed: () => {},
+                                      child: const Text(
+                                        "Remove",
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 234, 228, 228),
+                                            fontFamily: 'Comfortaa',
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 14),
+                                      )),
+                                ),
+                              ]),
+                        );
+                      })),
+            ],
+          ),
         ),
       ),
     ]));
