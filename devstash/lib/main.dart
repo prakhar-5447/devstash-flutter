85fd8ee3ae0476125dee0845a9f2c07163aaa2d5
pratham-094 <sahupratham022003@gmail.com>
2023-07-16 14:59:20 +0530

    test method implementation

--- devstash/lib/main.dart
+++ devstash/lib/main.dart
@@ -1,17 +1,47 @@
+import 'package:devstash/models/response/userResponse.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer';
+
+// screens
+import 'package:devstash/models/response/projectResponse.dart';
 import 'package:devstash/screens/saved.dart';
 import 'package:devstash/screens/project.dart';
 import 'package:devstash/screens/projectDetailScreen.dart';
 import 'package:devstash/screens/welcome.dart';
 import 'package:devstash/screens/HomeScreen.dart';
 import 'package:devstash/screens/ProfileScreen.dart';
-import 'package:flutter/material.dart';
+
+// services
+import 'package:devstash/services/projectServices.dart';
+import 'package:devstash/services/userServices.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late UserResponse? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    _user = (await UserServices().getUser());
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {
+          log(_user.toString());
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
