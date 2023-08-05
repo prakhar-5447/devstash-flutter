92e08c275cefec24f61f870c629fc78c1ac32188
pratham-094 <sahupratham022003@gmail.com>
2023-08-05 16:11:44 +0530

    schema modified

--- devstash/lib/models/ProjectList.dart
+++ devstash/lib/models/ProjectList.dart
@@ -1,7 +1,11 @@
 class ProjectList {
+  String _id;
   String _image;
 
-  ProjectList(this._image);
+  ProjectList(this._id, this._image);
+
+  String get id => _id;
+  set id(String value) => _id = value;
 
   String get image => _image;
   set image(String value) => _image = value;
